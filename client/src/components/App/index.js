dce574dc6165af1234fb855f2a2989aaa40e475c
Xiaoyan Liao <running923@gmail.com>
2018-07-03 10:12:54 -0700

    feature: add input rules

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -7,7 +7,14 @@ class App extends Component {
     return (
       <div className="App">
         <Table />
+        <article className='text-left ml-5 mt-5'>
+          <h5 className='text-danger'>* Accepted Input</h5>
+          <p>1. number, string</p>
+          <p>2. cell addition like '=A1+B1'</p>
+          <p>3. numeric addition like '=1+0.2'</p>
+        </article>
       </div>
+
     );
   }
 }
